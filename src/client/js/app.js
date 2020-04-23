ad86d84550057bec15d2e1060d49e98ac7dd4b94
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-04-23 15:14:01 -0400

    Added helper functions to app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -8,11 +8,25 @@ function performAction(e){
             console.log(response);
             document.getElementById('location').innerHTML = `Location: ${response.cityName}, ${response.stateCode} ${response.countryCode}`;
             document.getElementById('date').innerHTML = `Date of Departure: ${response.forecastDate}`;
-            document.getElementById('weather').innerHTML = `Temperature High: ${response.highTemp} Temperature Low: ${response.lowTemp}`;
+            document.getElementById('weather').innerHTML = `Temperature High: ${celsiusToFahrenheit(response.highTemp)} Temperature Low: ${celsiusToFahrenheit(response.lowTemp)}`;
         }
     )
 }
 
+
+function daysInFuture() {
+    let str = document.getElementById('inputDate');
+    let start = new Date(str);
+    let now = new Date();
+    let diffMillis = start.getTime() - now.getTime();
+    let diffDays = Math.floor(diffMillis/(1000 * 60 * 60 * 24));
+}
+
+
+function celsiusToFahrenheit(celsius) {
+    let f = celsius * 9 / 5 + 32;
+    return f;
+}
 
 const getData = async (url = '')=>{
     const response = await fetch(url);
