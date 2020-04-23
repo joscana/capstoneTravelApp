ce552dd8cb4b23c3c0d84330c16b9d4d762c5e7f
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-04-23 15:45:59 -0400

    Trip Countdown working

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,6 +1,7 @@
 function performAction(e){
     const city = document.getElementById('city').value;
     const date = document.getElementById('inputDate').value;
+    const daysUntilTrip = daysInFuture(date);
     const url = `http://localhost:3000/get?city=${city}`
     const encodedUrl = encodeURI(url);
     getData(encodedUrl)
@@ -9,18 +10,19 @@ function performAction(e){
             console.log(response);
             document.getElementById('location').innerHTML = `Location: ${response.cityName}, ${response.stateCode} ${response.countryCode}`;
             document.getElementById('date').innerHTML = `Date of Departure: ${date}`;
+            document.getElementById('countdown').innerHTML = `Days until Trip: ${daysUntilTrip}`
             document.getElementById('weather').innerHTML = `Temperature High: ${celsiusToFahrenheit(response.highTemp)} Temperature Low: ${celsiusToFahrenheit(response.lowTemp)}`;
         }
     )
 }
 
 
-function daysInFuture() {
-    let str = document.getElementById('inputDate');
-    let start = new Date(str);
+function daysInFuture(date) {
+    let start = new Date(date);
     let now = new Date();
     let diffMillis = start.getTime() - now.getTime();
     let diffDays = Math.floor(diffMillis/(1000 * 60 * 60 * 24));
+    return diffDays;
 }
 
 
