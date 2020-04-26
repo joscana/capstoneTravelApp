c26d23e4c618d3818b4cec4552891c8f575933e8
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-04-26 19:28:07 -0400

    Changes to scss styling

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import './styles/main.scss'
 import './styles/button.scss'
+import './styles/cards.scss'
 import './styles/header.scss'
 import './styles/textfields.scss'
 import { performAction } from './js/app'
