3d7d0d2da47e0db407525909d57c5363cca7c82e
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-04-26 15:09:51 -0400

    DOM displays trip length

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,7 +1,9 @@
 function performAction(e){
     const city = document.getElementById('city').value;
-    const date = document.getElementById('inputDate').value;
-    const daysUntilTrip = daysInFuture(date);
+    const startDate = document.getElementById('inputDate').value;
+    const endDate = document.getElementById('endDate').value;
+    const daysUntilTrip = daysInFuture(startDate);
+    const tripLength = daysBetween(startDate, endDate);
     const url = `http://localhost:3000/get?city=${city}&daysUntilTrip=${daysUntilTrip}`
     const encodedUrl = encodeURI(url);
     getData(encodedUrl)
@@ -9,8 +11,9 @@ function performAction(e){
         function(response) {
             console.log(response);
             document.getElementById('location').innerHTML = `Location: ${response.cityName}, ${response.stateCode} ${response.countryCode}`;
-            document.getElementById('date').innerHTML = `Date of Departure: ${date}`;
-            document.getElementById('countdown').innerHTML = `Days until Trip: ${daysUntilTrip}`
+            document.getElementById('date').innerHTML = `Date of Departure: ${startDate}`;
+            document.getElementById('countdown').innerHTML = `Days until Trip: ${daysUntilTrip}`;
+            document.getElementById('tripLength').innerHTML = `Trip Length: ${tripLength} days`
             document.getElementById('weather').innerHTML = `Temperature High: ${celsiusToFahrenheit(response.highTemp)} Temperature Low: ${celsiusToFahrenheit(response.lowTemp)}`;
             let image = document.createElement("img");
             image.src = response.imageURL;
@@ -26,6 +29,15 @@ function daysInFuture(date) {
     let start = new Date(date);
     let now = new Date();
     let diffMillis = start.getTime() - now.getTime();
+    let diffDays = Math.ceil(diffMillis/(1000 * 60 * 60 * 24));
+    return diffDays;
+}
+
+
+function daysBetween(startDate, endDate) {
+    let start = new Date(startDate);
+    let end = new Date(endDate);
+    let diffMillis = end.getTime() - start.getTime();
     let diffDays = Math.ceil(diffMillis/(1000 * 60 * 60 * 24));
     return diffDays;
 }
